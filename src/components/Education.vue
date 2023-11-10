df3a2a56225d6bcd795a1219d484cc8ac99889e1
Lucas Magno <lucasamagno@gmail.com>
2023-11-10 14:26:54 -0500

    add content for work and education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,5 +3,31 @@ console.log("HEY YOU FOUND ME :)")
 </script>
 
 <template> 
-<h1>Education</h1>
+<h1>EDUCATION</h1>
+
+<h2>M.S. MECHANICAL ENGINEERING</h2>
+<h3>Noise And Vibration Control</h3>
+<h4>MAY 2021</h4>
+<h3>L'Aquila, Italy</h3>
+<h4>SEPTEMBER 2019 - JANUARY 2020</h4>
+<p>Offered a scholarship to study abroad in Italy by Dr. Chalhoub, head of the Mechanical Engineering department.
+I was to be the first student to pilot this new study abroad program and give feedback on all the pain points to help smooth the process for future students.
+Studied Advanced Control Systems and Python. 
+ I'm Extremely grateful for the opportunity </p>
+
+
+<h2>BACHELORS DEGREE</h2>
+<h4>December 2018</h4>
+
+<h3>Sinusoidal Engine</h3>
+<h4>Advanced CAD Project (ME 5580)</h4>
+<p>Designed a conceptual novel design of an engine combining the sinusoidal and duke engines using NX.
+Conducted CAE (FEA, CFD, and thermal) analysis on the parts in addition to simulating it in NX.</p>
+
+<h3>Portable Water Filtration System</h3>
+<h4>Senior Design Project (ME 4500)</h4>
+<p>This project was intended to tackle the issue of water scarcity in underdeveloped regions of the world. We developed a replicable process to develop a cheap and portable water filtration system that can be taken from village to village easily or even transport the water a reasonable distance.
+the system uses a three stage filtration process that could clean and sanitize natural water using a dual stage particulate filter, carbon filter, and a UV filter..
+We cut open an electric pump which was attached to the stand in such a way to pump the water only when the bicycle was in the pumping configuration. then attached it to the filters which were placed in a balanced configuration when transporting water. An added option was the UV filter which we were unsure would be feasible in the target communities yet if possible was highly recommended. For the UV filter two Dynamos and a small battery, to ensure consistent power delivery for the filter, were added.</p>
+
 </template> 
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -3,5 +3,96 @@ console.log("HEY YOU FOUND ME :)")
 </script>
 
 <template> 
-<h1>Work Experience</h1>
+<h1>WORK EXPERIENCE</h1>
+
+<h2>REGIONAL SAFETY MANAGER</h2>
+<h3>MAY 2022 – PRESENT</h3>
+<h3>BOSCH</h3>
+<p>Supervisor: Tushar Chaudhari</p>
+<h4>Responsible for overall Function Safety Management in context of Engineering of Bosch Automotive Steering Systems.</h4>
+<p>Ensure Safety and security of Bosch electric steering systems by adhering to ISO26262
+Create and update the hazard and risk analyses, safety plan, functional and technical safety concept
+Support the definition/specification of safety requirements for hardware and software architectures
+Lead support Safety Assessment Reviews held by safety assessor
+Responsible for most GM projects in North America</p>
+
+
+<h2>CONSULTANT</h2>
+<h3>JULY 2021 – MAY 2022</h3>
+<h3>INVENSITY</h3>
+<p>Supervisor: Tristan Eggenberger</p>
+<h4>Ensure client satisfaction by performing the assigned tasks as well as correcting and resolving any issues that may crop up.</h4>
+<h3>REQUIREMENTS ENGINEERING</h3>
+<p>Analysis and assigning incoming stakeholder requirements
+Creating and maintaining traceability between system architecture and system requirement
+Decomposition of requirements</p>
+<h3>TECHNICAL PROJECT MANAGEMENT</h3>
+<p>Leading engineers from multiple engineering disciplines to develop & deliver automotive driveline mechatronics systems.
+Working across multiple disciplines in System V development process
+Developing driveline products incorporating automotive industry standards & processes including ASPICE, ISO26262.
+Utilizing project planning, scope break down & allocation, issue management & resolution, with Agile principles utilizing IBM RTC</p>
+
+
+<h2>INNOVATION CONSULTANT</h2>
+<h3>MAY 2021 – FEBRUARY 2022</h3>
+<h3>INNOVATRIUM</h3>
+<p>Supervisors: Dr. Staney DeGraff, Dr. Jeff GeGraff</p>
+<h4>Working with Air National Guard and Air Force members to hold in-person coachings for AIMHI and Project Mercury as well as launching Project DAWG</h4>
+<p>AIMHI and Project Mercury aim to develop Innovation hubs within the Air National Guard and Air Force respectively. 
+Project DAWG (Developing Airmen With Games) is a student game development competition that's currently in progress and due to be completed by the end of the end of summer.</p>
+
+
+<h2>FLEET SYSTEMS OPERATOR</h2>
+<h3>JANUARY 2021 – JULY 2021</h3>
+<h3>WAYMO</h3>
+<p>Supervisor: Mazen Sarsour</p>
+<h4>Assess and analyze real world and simulated situations to support vehicle operations in the field at Waymo via Genesis10.</h4>
+<p>Analyze various situations the car encounters and provide feedback to the engineering team to allow for software improvement
+Ensure smooth day to day operations of a fleet of autonomous vehicles.
+Detect, communicate, and escalate potential issues to appropriate parties.
+Work with engineers to enhance the software of autonomous vehicles.</p>
+
+
+<h2>RESEARCH ASSISTANT</h2>
+<h3>MAY 2019 - SEPTEMBER 2019</h3>
+<h3>Wayne State University</h3>
+<p>Supervisor: Dr. Marcis Jansons</p>
+<h4>Conducting engine research on behalf TARDEC and Toyota. Then, converted the diesel research engine to gasoline.</h4>
+<p>Conducted calibration testing for all components using LabVIEW and MATLAB.
+Studied the effects of water injection in the combustion chamber of diesel engines by conducting highspeed imaging using LabVIEW and a highspeed thermal camera.
+Converted an AVL optical engine (15 of in the U.S.) from a CI diesel to the only SI gasoline engine of its kind. For which I studied engine design and mated it to a one-of-a-kind cylinder head.
+Designed new components and assemblies using Fusion360 and UG/NX.</p>
+
+
+<h2>PRODUCT ENGINEERING CO-OP</h2>
+<h3>SEPTEMBER 2018 – DECEMBER 2018</h3>
+<h3>ZF-TRW</h3>
+<p>Supervisor: Mark Mieloch</p>
+<h4>Worked in the Foundation Braking department on FCA vehicles under Mr. Mark Mieloch</h4>
+<p>Assisted the FCA team on multiple projects, mainly the Dodge Ram HD and the Jeep Wrangler.
+Analyzed test reports and created internal and external DVP&Rs.
+Shared ownership of the Wrangler project with one other engineer, where I maintained full ownership of the front brakes testing.
+Conducted frequency spectrum analysis testing on drum brakes by collecting data to be analyzed using Fast Fourier Transform (FFT)
+Was given full ownership of a project which I developed a test plan for</p>
+
+
+<h2>QUALITY ENGINEERING INTERN</h2>
+<h3>JUNE 2018 – AUGUST 2018</h3>
+<h3>XENITH</h3>
+<p>Supervisor: David Marten</p>
+<p>I worked along side Mohammad Ali Al-Ameen and David Marten to form the quality engineering team.
+ My duties included handling warranty claims, Inspecting incoming shipments, quality control of outgoing shipments, in addition to assisting assembly line workers wherever needed.
+While there I developed a new warranty system to generate data that could be used to inform design updates or upcoming products. In addition, I conducted a study to address assembly inconsistencies which highlighted a tolerance issue.</p>
+
+
+
+<h2>INNOVATION FELLOW</h2>
+<h3>NOVEMBER 2017 - JULY 2018</h3>
+<h3>INNOVATRIUM</h3>
+<p>Supervisors: Dr. Ethan Eagle, Dr. Jim Fish, Dr. Staney DeGraff</p>
+<p>Assisted Dr. Ethan Eagle and Dr. Jim Fish on projects with Bosch then ITC. 
+Coordinated teams and guided team members on starting new innovative projects over a three month period until they presented their funding proposals.
+Conducted market research and analysis to assist the teams in maintaining their deadlines. 
+Monitored team progression and made suggestions to guide their progress.</p>
+
 </template> 
