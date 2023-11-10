efb03dd8534bc039794e76c74eca12a59549b30f
Lucas Magno <lucasamagno@gmail.com>
2023-11-10 12:18:08 -0500

    new update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,5 +3,14 @@
 </script>
 
 <template> 
-    <h1>Mo Hijazi </h1>
+    <h1>Mo Hijazi</h1>
+    <h2>I'm just curious</h2>
+
+    <button>Resume</button>
+    <button>Work Experience</button>
+    <button>Education</button>
+
+    <button>LinkedIn</button>
+    <button>Instagram</button>
+    <button>Email</button>
 </template> 
